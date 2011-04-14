c2553bc528d062f78168e500334c647e37f19dcd
Adam Miribyan <kreditmarket.com@gmail.com>
2011-04-14 13:17:27 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+gem 'mysql2'
+
+gem 'less'
+gem 'simple_form'
+gem 'russian'
+gem 'high_voltage'
+
+gem 'mini_magick'
+gem 'carrierwave'
+
+gem 'jquery-rails', '~>0.2'
+
+group :development do
+  gem 'capistrano'
+  gem 'thin'
+end
+
+
+group :development, :test do
+  gem 'mocha'
+  gem 'factory_girl_rails'
+  gem 'autotest'
+  gem 'autotest-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bree::Application.config.secret_token = '34c7563cda06e05422922d1518cb39179ab05ff2630586dab960cb98fbe37940fb6dfb53dd70177768c9481fa047885504bde9a697c845f0b30137e1e6bd14f5'
