de307b9237933784f87c6c67d9f7423643257ed6
Adam Miribyan <kreditmarket.com@gmail.com>
2011-05-07 05:08:13 +0400

    added a birth date field for a doctor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110507002332) do
+ActiveRecord::Schema.define(:version => 20110507010432) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20110507002332) do
     t.string   "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "birth_date"
   end
 
   create_table "patients", :force => true do |t|
