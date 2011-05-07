fcc1e45335c509f12bb1995b4b5acf3cae4b636c
Adam Miribyan <adam.miribyan@gmail.com>
2011-05-07 22:37:46 +0400

    comments scaffold and associations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < ActiveRecord::Base
   has_one :teeth_chart
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bree::Application.routes.draw do
+  resources :comments
+
   resources :doctors
 
   resources :services
