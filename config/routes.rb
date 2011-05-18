07bfa65b02ace34c1d195437c62bf56185cb5f0d
Adam Miribyan <adam.miribyan@gmail.com>
2011-05-18 14:09:36 +0400

    changed the root route to catalog index

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Bree::Application.routes.draw do
   resources :comments
-
   resources :doctors
-
   resources :services
-
   resources :categories
-
-  get "teeth_charts/update"
-
-  get "application/autocomplete_patient_full_name"
-  
   resources :patients
   resources :teeth_charts
 
-  root :to => "patients#new"
+  get "teeth_charts/update"
+  get "application/autocomplete_patient_full_name"  
+
+  root :to => "categories#index"
 end
