0416aadcf2cea75c90b2b5c1846afe74a1b8c269
Adam Miribyan <adam.miribyan@gmail.com>
2011-05-23 03:26:24 +0400

    template scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110517130506) do
+ActiveRecord::Schema.define(:version => 20110522232410) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -106,6 +106,14 @@ ActiveRecord::Schema.define(:version => 20110517130506) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "patient_id"
+  end
+
+  create_table "templates", :force => true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "doctor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
