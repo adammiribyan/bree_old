962ecaa70f8fcfaccfb338d6ce16710fdaffa596
Adam Miribyan <adam.miribyan@gmail.com>
2011-05-23 09:01:57 +0400

    removed doctor_id from templates table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110523012241) do
+ActiveRecord::Schema.define(:version => 20110523045952) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(:version => 20110523012241) do
   create_table "templates", :force => true do |t|
     t.string   "name"
     t.text     "body"
-    t.integer  "doctor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "has_teeth_chart"
