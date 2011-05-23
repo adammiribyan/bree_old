86e70cc0d712f04fe634909f5007199cf0ba05e6
Adam Miribyan <adam.miribyan@gmail.com>
2011-05-24 01:31:08 +0400

    routes for appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Bree::Application.routes.draw do
-  resources :appointments
-
   resources :templates, :except => :show
   resources :comments
   resources :doctors
   resources :services
-  resources :categories
-  resources :patients
+  resources :categories  
   resources :teeth_charts
+  resources :patients, :shallow => true do
+    resources :appointments
+  end
 
   get "teeth_charts/update"
   get "application/autocomplete_patient_full_name"  
